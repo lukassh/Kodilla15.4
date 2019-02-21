1e13e29889410023b7cf3141e35282dd02b4f1d4
Lukass <lukassh@gmail.com>
2019-02-21 14:14:09 +0100

    Fix errors

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 function Phone(brand, price, color, weight, screen, memory, model, inrank) {
+	
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
-  this.weight = weight;
-  this.screen = screen;
-  this.memory = memory;
-  this.model = model;
-  this.rank = inrank;
-  this.logModel = function() {
-		console.log('This is ' + this.brand +' '+ this.model);
-	};
-  this.logRanking = function() {
-		console.log(this.brand +' '+ this.model + ' is '+ this.rank + ' in ranking.');
-	}
-  
+  	
+  	this.weight = weight;
+  	this.screen = screen;
+  	this.memory = memory;
+  	this.model = model;
+  	this.rank = inrank; 
 }
+
 
 Phone.prototype.printInfo = function() {
 	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '. Specyfication: ' + this.weight + ' gram, ' + this.screen + ' resolution and ' + this.memory + ' gb memory.');
 };
+
+Phone.prototype.logRanking = function() {
+	console.log(this.brand +' '+ this.model + ' is '+ this.rank + ' in ranking.');
+};
+
+Phone.prototype.logModel = function() {
+	console.log('This is ' + this.brand +' '+ this.model);
+}
 
 var Iphone6s = new Phone("Apple", 2250, "silver", 200, "600x480", 2, 'Iphone 6S', 1);
 var GalaxyS6 = new Phone('Samsung', 3100, 'Black', 300, "640x4840", 3, 'S6', 2);
